90c910070af245b4e4b6c4da3c0f8c8b604fc4ef
Fernanda López <fer.daye.2007@gmail.com>
2023-10-31 11:18:58 -0600

    🌹 Song context

--- PLAYLIST/src/components/context/SongContext.jsx
+++ PLAYLIST/src/components/context/SongContext.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from 'react'
-//Importacion de la basa que datos que contiene JSON 
+import React, { useEffect, useState, createContext } from 'react'
+// Importacion de la basa que datos que contiene JSON
 import canciones from '@/assets/listaCanciones.json'
-import React, { createContext } from 'react'
 
-const songContext = React.createContext(
-  { count: 0, setCount: () => {} }
-)
+const SongContext = createContext()
+
 function SongProvider ({ children }) {
-  const [list, setList] = useState([]) // lista de canciones 
-  const [loading, setLoading] = useState(true) //está cargando?
-  const [selectedSong, setSelectedSong]= useState({}) // que cancion se selecciono?
+  const [list, setList] = useState([]) // lista de canciones
+  const [loading, setLoading] = useState(true) // está cargando?
+  const [selectedSong, setSelectedSong] = useState({}) // que cancion se selecciono?
 
-  //simulacion a la llamada de la API
+  // simulacion a la llamada de la API
   useEffect(() => {
     setTimeout(() => {
       setList(canciones)
       setLoading(false)
     }, 2000)
   }, [])
+  const data = {
+    list,
+    loading,
+    selectedSong,
+    setSelectedSong
+  }
+  return (
+    <SongContext.Provider value={data}>
+      {children}
+    </SongContext.Provider>
+  )
+}
+
+export {
+  SongProvider,
+  SongContext
 }
--- PLAYLIST/src/pages/Home/Home.jsx
+++ PLAYLIST/src/pages/Home/Home.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import SongList from '@/components/SongList/SongList'
 import SongDetails from '@/components/SongDetails/SongDetails'
+import { SongProvider } from '@/components/context/SongContext'
 import './Home.css'
 
 const Home = () => {
   return (
     <>
-      <div className='home-conteiner'>
-        <div className='izquierdo'>
-          <h2>Izquierdo</h2>
-          <SongList />
+      <SongProvider>
+        <div className='home-conteiner'>
+          <div className='izquierdo'>
+            <h2>Izquierdo</h2>
+            <SongList />
+          </div>
+          <div className='derecho'>
+            <h2>Derecho</h2>
+            <SongDetails />
+          </div>
         </div>
-        <div className='derecho'>
-          <h2>Derecho</h2>
-          <SongDetails />
-        </div>
-      </div>
+      </SongProvider>
     </>
   )
 }
