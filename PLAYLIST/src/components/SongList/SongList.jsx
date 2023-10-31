daf9bd059b6be6be168a16349159e5b0d6da3a87
Fernanda López <fer.daye.2007@gmail.com>
2023-10-30 19:49:51 -0600

    😎 canciones

--- PLAYLIST/src/components/SongList/SongList.jsx
+++ PLAYLIST/src/components/SongList/SongList.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './SongList.css'
+import canciones from '@/assets/listaCanciones.json'
 
 const SongList = () => {
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setList(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
   return (
     <>
+      <section className='row-container'>
+        {loading
+
+          ? <h1>Cargando</h1>
+          : list.map((song) => (
+            <div className='row-song' key={song.id}>
+              <h2>{song.title}</h2>
+              <h3>{song.artist}</h3>
+            </div>
+          )
+
+          )}
+      </section>
 
     </>
   )
--- PLAYLIST/src/pages/Home/Home.jsx
+++ PLAYLIST/src/pages/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import SongList from '../../components/SongList/SongList'
+import './Home.css'
 
 const Home = () => {
   return (
     <>
-      <p>hola</p>
+      <div className='home-conteiner'>
+        <div className='izquierdo'>
+          <h2>Izquierdo</h2>
+          <SongList />
+        </div>
+        <div className='derecho'>
+          <h2>Derecho</h2>
+          <div>Lorequaerat omni repellendus magnam saepe maximeorequaerat omni repellendus magnam saepe maximeorequaerat omni repellendus magnam saepe maxime.</div>
+        </div>
+      </div>
     </>
   )
 }
