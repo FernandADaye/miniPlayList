eab0e396b5981409c5e944e5a4df8549ef36646f
Fernanda López <fer.daye.2007@gmail.com>
2023-10-31 19:25:01 -0600

    🤞💕 contexto finalizado

--- PLAYLIST/src/components/SongList/SongList.jsx
+++ PLAYLIST/src/components/SongList/SongList.jsx
@@ -2,14 +2,18 @@ import './SongList.css'
 import { useSongContext } from '../hooks/useSongContext'
 
 const SongList = () => {
-  const { list, loading } = useSongContext()
+  const { list, loading, setSelectedSong } = useSongContext()
   return (
     <>
       <section className='row-container'>
         {loading
           ? <h1>Cargando</h1>
           : list.map((song) => (
-            <div className='row-song' key={song.id}>
+            <div
+              className='row-song'
+              key={song.id}
+              onClick={() => setSelectedSong(song)}
+            >
               <h2>{song.title}</h2>
               <h3>{song.artist}</h3>
             </div>
--- PLAYLIST/src/pages/Home/Home.jsx
+++ PLAYLIST/src/pages/Home/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
           </div>
           <div className='derecho'>
             <h2>Derecho</h2>
-            <br />
             <SongDetails />
           </div>
         </div>
