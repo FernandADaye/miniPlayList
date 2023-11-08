772075711875d2b0dbafcf9997840598fdf95dc6
Fernanda López <fer.daye.2007@gmail.com>
2023-11-07 20:15:03 -0600

    Bem/SASS navbar

--- Proyect-Kata/Proyect-Kata/src/component/Header/Header.jsx
+++ Proyect-Kata/Proyect-Kata/src/component/Header/Header.jsx
@@ -4,20 +4,20 @@ import './header.scss'
 const Header = () => {
   return (
     <>
-      <nav className='header__nav'>
+      <nav className='header'>
         <a className='header__nav-logo' href='/'>Logo</a>
         <ul className='header__nav-lista'>
           <li className='header__nav-lista-elemento'>
-            <a className='lista-elemento-a' href='/Home'>Home</a>
+            <a href='/Home' className='header__lista-elemento-a'>Home</a>
           </li>
           <li className='header__nav-lista-elemento'>
-            <a className='lista-elemento-a' href='/Dashboard'>Dashboard</a>
+            <a className='header__lista-elemento-a' href='/Dashboard'>Dashboard</a>
           </li>
           <li className='header__nav-lista-elemento'>
-            <a className='lista-elemento-a' href='/Login'>Login</a>
+            <a className='header__lista-elemento-a' href='/Login'>Login</a>
           </li>
           <li className='header__nav-lista-elemento'>
-            <a className='lista-elemento-a' href='/Singup'>Singup</a>
+            <a className='header__lista-elemento-a' href='/Singup'>Singup</a>
           </li>
 
         </ul>
