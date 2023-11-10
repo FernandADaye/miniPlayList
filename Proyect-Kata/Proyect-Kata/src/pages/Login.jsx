e6298bf80979b11123fae9bd515e36a28d26a5bf
Fernanda López <fer.daye.2007@gmail.com>
2023-11-09 20:45:00 -0600

    Login Page 🕶

--- Proyect-Kata/Proyect-Kata/src/pages/Login.jsx
+++ Proyect-Kata/Proyect-Kata/src/pages/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './pages.scss'
 
 const Login = () => {
   return (
     <>
-      <h1>Login </h1>
+      <div className='form-container'>
+        <main className='form-signin w-100 m-auto'>
+          <form>
+            <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
+            <div className='form-floating'>
+              <input
+                type='email'
+                className='form-control'
+                id='floatingInput'
+                placeholder='name@example.com'
+              />
+              <label htmlFor='floatingInput'>Email address</label>
+            </div>
+            <div className='form-floating'>
+              <input
+                type='password'
+                className='form-control'
+                id='floatingPassword'
+                placeholder='Password'
+              />
+              <label htmlFor='floatingPassword'>Password</label>
+            </div>
+            <div className='form-check text-start my-3' />
+            <button className='btn btn-primary w-100 py-2' type='submit'>
+              Sign in
+            </button>
+          </form>
+        </main>
+      </div>
     </>
   )
 }
