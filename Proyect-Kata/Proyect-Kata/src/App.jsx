4f0cdeed645d63deebbca04fb691ca7f58cd20b9
Fernanda López <fer.daye.2007@gmail.com>
2023-11-17 09:28:35 -0600

    context and provider ,,,😋

--- Proyect-Kata/Proyect-Kata/src/App.jsx
+++ Proyect-Kata/Proyect-Kata/src/App.jsx
@@ -3,15 +3,18 @@ import { Header } from '@/component/Header'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import Routesindex from './routers/Routesindex'
+import { AuthProvider } from './contex/AuthContext'
 
 function App () {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        Principal
-        <Routesindex />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          Principal
+          <Routesindex />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- Proyect-Kata/Proyect-Kata/src/contex/AuthContext.jsx
+++ Proyect-Kata/Proyect-Kata/src/contex/AuthContext.jsx
@@ -1,25 +1,47 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { jwtDecode } from 'jwt-decode'
 
 const AuthContext = createContext()
-const [isAuth, setIsAuth] = useState(false)
-const [userPlayLoad, setuserPlayLoad] = useState(null)
+// const [isAuth, setIsAuth] = useState(false)
+// const [userPlayLoad, setuserPlayLoad] = useState(null)
 
 const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false)
+  const [userPlayLoad, setuserPlayLoad] = useState(null)
+
   const loding = (token) => {
     localStorage.setItem('token', token)
-    const decode= jwtDecode(token)
+    const decode = jwtDecode(token)
     setuserPlayLoad(decode)
     setIsAuth(true)
   }
-  const logout=()=>{
+  const logout = () => {
     localStorage.removeItem('token')
     setuserPlayLoad(null)
     setIsAuth(false)
+  }
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const decode = jwtDecode(token)
+      setuserPlayLoad(decode)
+      setIsAuth(true)
+    }
+  }, [])
+  const data = {
+    isAuth,
+    userPlayLoad,
+    loding,
+    logout
   }
   return (
     <AuthContext.Provider value={{}}>
       {children}
     </AuthContext.Provider>
   )
+}
+
+export {
+  AuthContext,
+  AuthProvider
 }
