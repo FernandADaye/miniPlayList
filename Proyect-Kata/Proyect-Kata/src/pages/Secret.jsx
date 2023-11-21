d4b28225809cfd4b78f581bb1de606bcffcecf82
Fernanda López <fer.daye.2007@gmail.com>
2023-11-21 14:13:45 -0600

    Renderizado de cada Usuario 🦋

--- Proyect-Kata/Proyect-Kata/src/pages/Secret.jsx
+++ Proyect-Kata/Proyect-Kata/src/pages/Secret.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useAuthContext } from '@/hooks/useAuthContext'
 
 const Secret = () => {
+  const { userPlayLoad } = useAuthContext()
   return (
     <>
       <h1>Secret </h1>
+      {userPlayLoad?.role === 'ADMIN'
+        ? <h2>Hola Admin</h2>
+        : <h2>Hola Costomer</h2>}
     </>
   )
 }
