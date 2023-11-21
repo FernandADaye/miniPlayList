a91e9d3c8e9793f65e4a99cd772a5bc4ecb97000
Fernanda López <fer.daye.2007@gmail.com>
2023-11-21 12:20:51 -0600

    Navigate🐱‍👤

--- Proyect-Kata/Proyect-Kata/src/App.jsx
+++ Proyect-Kata/Proyect-Kata/src/App.jsx
@@ -2,7 +2,7 @@ import { Header } from '@/component/Header'
 
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-import Routesindex from './routers/Routesindex'
+import Routesindex from './routers/RoutesIndex'
 import { AuthProvider } from './context/AuthContext'
 
 function App () {
--- Proyect-Kata/Proyect-Kata/src/routers/RoutesIndex.jsx
+++ Proyect-Kata/Proyect-Kata/src/routers/RoutesIndex.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useAuthContext } from '@/hooks/useAuthContext'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Dashboard, Home, Login, Secret, Singup } from '@/pages'
 
 const Routesindex = () => {
+  const { isAuth } = useAuthContext()
   return (
     <Routes>
       <Route path='/home' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/singup' element={<Singup />} />
-      <Route path='/secret' element={<Secret />} />
+      <Route
+        path='/secret'
+        element={isAuth ? <Secret /> : <Navigate to='/login' />}
+      />
     </Routes>
   )
 }
