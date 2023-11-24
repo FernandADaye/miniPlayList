3d5dfcb221baa89f59687de684fab38266601ebc
Fernanda López <fer.daye.2007@gmail.com>
2023-11-23 21:09:12 -0600

    Cypress inicios 👌🏻

--- Proyect-Kata/Proyect-Kata/.eslintrc.cjs
+++ Proyect-Kata/Proyect-Kata/.eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'standard',
-    'standard-jsx'
+    'standard-jsx',
+    "plugin:cypress/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -20,4 +21,5 @@ module.exports = {
     ],
     'react/prop-types':'off',
   },
+  
 }
