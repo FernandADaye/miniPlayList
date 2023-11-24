b273e19f1bc0efa72c9ce88901b47f53d7962ee7
Fernanda López <fer.daye.2007@gmail.com>
2023-11-23 21:56:18 -0600

    pruebas ✨✨✨

--- Proyect-Kata/Proyect-Kata/cypress/e2e/auth.cy.js
+++ Proyect-Kata/Proyect-Kata/cypress/e2e/auth.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Funcionabilidad De Login ', () => {
+  it('Carga Leyendo Home en /', () => {
+    cy.visit('/Home')
+    cy.get('h1')
+      .contains('Home')
   })
 })
